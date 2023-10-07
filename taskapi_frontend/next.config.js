23c70db667957fd1e27e8d14d4fe1724172ec432
Peter Kappelt <kappelt.peter@gmail.com>
2023-10-07 12:59:37 +0200

    Frontend: rewrite for API backend access

--- taskapi_frontend/next.config.js
+++ taskapi_frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: "/accounts/:path*/",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/accounts/:path*/`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
